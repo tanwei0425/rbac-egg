366387ec06139cf8e31b0c14881a574264a31e33
tanwei@jilinxiangyun.com <tanwei@jilinxiangyun.com>
2021-11-21 18:45:42 +0800

    兼容门户不加密传输的问题

--- app/middleware/decrypt.js
+++ app/middleware/decrypt.js
@@ -4,18 +4,22 @@ module.exports = () => {
     return async function decrypt(ctx, next) {
         const { helper, query, request } = ctx;
         if (JSON.stringify(ctx.request.body) !== '{}') {
-
-            let { data, rsa } = request.body;
-            // 解密
-            data = helper.decrypt(data);
-            if (!helper.equal(rsa, JSON.stringify(data))) {
-                helper.render(422);
-                return;
+            console.log(request.body, 'data, rsa');
+            let resData = request.body;
+            // 处理门户不需要加密的情况
+            if (resData.rsa) {
+                let { data, rsa } = request.body;
+                // 解密
+                data = helper.decrypt(data);
+                if (!helper.equal(rsa, JSON.stringify(data))) {
+                    helper.render(422);
+                    return;
+                }
+                resData = data;
             }
-
             // data中JSON对象的key值转换为下划线格式
-            data && helper.jsonTo_(data);
-            ctx.request.body = data;
+            resData && helper.jsonTo_(resData);
+            ctx.request.body = resData;
         }
         // query中JSON对象的key值转换为下划线格式
         query && helper.jsonTo_(query);
